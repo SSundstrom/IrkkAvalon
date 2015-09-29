dd7a66f3046ba30a9457405c4c05cbb5dac5ba88
SSundstrom <simsund@student.chalmers.se>
2015-09-29 11:14:13 +0200

    Testar

---
+++ Bot/src/com/company/game/Test/TestBot.java
@@ -0,0 +1,11 @@
+package com.company.game.Test;
+
+import com.company.MyBot;
+
+public class TestBot {
+    public static void main(String[] args) {
+//        MyBot bot = new MyBot("irc.chalmers.it", 22, "Botalon", "banned");
+        System.out.print("Hello!");
+
+    }
+}
